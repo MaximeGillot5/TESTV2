c7372d44aa55ca8e8c1b0d8b1e4b2faf7828364d
JulienDast <julien.dast44@gmail.com>
2023-06-17 19:02:52 +0200

    position textes footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,13 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer id='footer' className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100">
-      <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+      <div className="container items-center flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3">
           <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
-            <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-900">
-                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-              </svg>
-            </div>
             <span className="self-center text-2xl font-semibold">Sport<span id='text-orange'>ner</span></span>
           </a>
         </div>
-        <div id='footer-right' className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+        <div id='footer-right' className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-3">
 
           <div className="space-y-3">
             <h3 id='text-orange' className="tracki uppercase dark:text-gray-50">Company</h3>
@@ -40,7 +35,7 @@ const Footer = () => {
           </div>
           <div className="space-y-3">
             <div id='text-orange' className="uppercase dark:text-gray-50">Réseaux Sociaux</div>
-            <div className="flex justify-start space-x-3">
+            <div className="flex justify-center space-x-3">
               <a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
                 <i id='icons' class="fa-brands fa-square-github"></i>
               </a>
